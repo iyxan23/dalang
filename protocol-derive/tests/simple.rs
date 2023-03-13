73b3a255fc222ff3c574163e5c6c38cf53633a7d
Iyxan23 <nurihsanalghifari@gmail.com>
2023-03-13 11:45:09 +0700

    test(s/protocol-derive): write tests for `as_opcode` and `decode_payload`

--- protocol-derive/tests/simple.rs
+++ protocol-derive/tests/simple.rs
@@ -9,17 +9,39 @@ where Self: Sized {
     fn encode_payload(self) -> Vec<u8>;
 }
 
-#[derive(Packet)]
+#[derive(Debug, PartialEq, Packet)]
 enum MyProtocol {
     #[opcode(0x0)]
     VariantA,
     #[opcode(0x1)]
-    VariantB,
+    VariantB {
+        name: String
+    },
     #[opcode(0x2)]
     VariantC,
 }
 
 #[test]
-fn test() {
-    // let varianta = <MyProtocol::VariantA as Packet>
+fn as_opcode_test() {
+    let a = MyProtocol::VariantA;
+    let b = MyProtocol::VariantB { name: String::new() }; // dummy string
+    let c = MyProtocol::VariantC;
+
+    assert_eq!(a.as_opcode(), 0x0);
+    assert_eq!(b.as_opcode(), 0x1);
+    assert_eq!(c.as_opcode(), 0x2);
+}
+
+#[test]
+fn simple_decode_payload_test() {
+    // a payload of {"name": "loremipsum"}
+    let payload: [u8; 17] = [129, 164, 110, 97, 109, 101, 170, 108, 111, 114, 101, 109, 105, 112, 115, 117, 109];
+
+    let ret = MyProtocol::decode_packet(0x1, &payload).unwrap();
+    assert_eq!(
+        ret,
+        MyProtocol::VariantB {
+            name: String::from("loremipsum")
+        }
+    );
 }
