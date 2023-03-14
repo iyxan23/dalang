21fc08e4bb62c448df73f54d1f473596bab4e3d0
Iyxan23 <nurihsanalghifari@gmail.com>
2023-03-14 09:21:44 +0700

    test(s/protocol-derive): for unit and unnamed variants of dec/encoding

--- protocol-derive/tests/simple.rs
+++ protocol-derive/tests/simple.rs
@@ -33,7 +33,23 @@ fn as_opcode_test() {
 }
 
 #[test]
-fn simple_decode_payload_test() {
+// Tests that the payload given should be ignored because the variant
+// is a unit variant. It does not have any payload to be decoded.
+fn ignore_payload_decode_test() {
+    let payload: [u8; 3] = [0, 0, 0]; // payload can be anything since it will be ignored
+    let ret = MyProtocol::decode_packet(0x0, &payload).unwrap();
+    assert_eq!(ret, MyProtocol::VariantA);
+}
+
+#[test]
+fn empty_payload_encode_test() {
+    let expected: [u8; 0] = []; // payload can be anything since it will be ignored
+    let payload = MyProtocol::encode_payload(MyProtocol::VariantA).unwrap();
+    assert_eq!(payload, expected);
+}
+
+#[test]
+fn named_decode_payload_test() {
     // a payload of {"name": "loremipsum"}
     let payload: [u8; 17] = [129, 164, 110, 97, 109, 101, 170, 108, 111, 114, 101, 109, 105, 112, 115, 117, 109];
 
@@ -47,10 +63,32 @@ fn simple_decode_payload_test() {
 }
 
 #[test]
-fn simple_encode_payload_test() {
+fn unnamed_decode_payload_test() {
+    // a payload of ["foo", "bar"]
+    let payload: [u8; 9] = [146, 163, 102, 111, 111, 163, 98, 97, 114];
+
+    let ret = MyProtocol::decode_packet(0x2, &payload).unwrap();
+    assert_eq!(
+        ret,
+        MyProtocol::VariantC("foo".to_string(), "bar".to_string())
+    );
+}
+
+#[test]
+fn named_encode_payload_test() {
     let expected: [u8; 17] = [129, 164, 110, 97, 109, 101, 170, 108, 111, 114, 101, 109, 105, 112, 115, 117, 109];
 
     let packet = MyProtocol::VariantB { name: String::from("loremipsum") };
+    let encoded = packet.encode_payload().unwrap();
+
+    assert_eq!(encoded, expected);
+}
+
+#[test]
+fn unnamed_encode_payload_test() {
+    let expected: [u8; 9] = [146, 163, 102, 111, 111, 163, 98, 97, 114];
+
+    let packet = MyProtocol::VariantC("foo".to_string(), "bar".to_string());
     let encoded = packet.encode_payload().unwrap();
 
     assert_eq!(encoded, expected);
